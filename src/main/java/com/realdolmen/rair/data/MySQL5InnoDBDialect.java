16f52dedf0172f275bd91a20c4bb16dd4aef7de0
Brent Couck <brent.couck@gmail.com>
2016-11-05 08:53:22 +0100

    Added modified Hibernate dialect

---
+++ src/main/java/com/realdolmen/rair/data/MySQL5InnoDBDialect.java
@@ -0,0 +1,20 @@
+package com.realdolmen.rair.data;
+
+import org.hibernate.MappingException;
+
+/**
+ * Modified dialect to prevent (meaningless) Hibernate error HHH000389.
+ * It is 'normal' behavior for Hibernate but it clutters the output. This is because it does not check if
+ * something exists before deleting it in the database.
+ */
+public class MySQL5InnoDBDialect extends org.hibernate.dialect.MySQL5InnoDBDialect {
+    @Override
+    protected String getDropSequenceString(String sequenceName) throws MappingException {
+        return "drop sequence if exists " + sequenceName;
+    }
+
+    @Override
+    public boolean dropConstraints() {
+        return false;
+    }
+}
