e80774aca73ed9f54a8cb4162a1071a1bd198078
Brent Couck <brent.couck@gmail.com>
2016-11-05 11:20:46 +0100

    Added example flight dao

---
+++ src/main/java/com/realdolmen/rair/data/FlightDao.java
@@ -0,0 +1,39 @@
+package com.realdolmen.rair.data;
+
+import com.realdolmen.rair.domain.entities.Flight;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@ApplicationScoped
+public class FlightDao {
+
+    @Inject
+    private EntityManager em;
+
+    @Transactional
+    public void registerFlight(Flight flight) {
+        em.persist(flight);
+    }
+
+    public List<Flight> getFlights() {
+        return em.createNamedQuery("Flight.findAll", Flight.class).getResultList();
+    }
+
+    @Transactional
+    public void deleteFlight(Flight flight) {
+        if(flight == null) {
+            throw new IllegalArgumentException("Flight cannot be null!");
+        }
+        Flight reference = em.getReference(Flight.class, flight);
+
+        if(reference == null) {
+            throw new NoSuchElementException(String.format("Flight #%s cannot be removed because it does not exist!", flight.getId()));
+        }
+        em.remove(reference);
+    }
+}
